350af818af543494650a839d4776600627b77d4e
VOID <102216483+void181@users.noreply.github.com>
2024-08-04 17:58:02 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -21,39 +21,47 @@
 import json
 import random
 import urllib.request
+import time
 
-# Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
-
-# 500 server request
-N = 500
-
+QUERY = "http://localhost:8080/query?id={}".format(random.random())
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ Produce all of the needed values to generate a datapoint """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None  # Avoid division by zero
+    return price_a / price_b
 
-
-# Main
-if __name__ == "__main__":
+def main():
+    """ Main entry point for the client. """
     # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+    N = 60
+    prices = {}
 
-        """ ----------- Update to get the ratio --------------- """
+    while True:
+        quotes = json.loads(urllib.request.urlopen(QUERY).read())
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price
+            print(f"Quoted {stock} at (bid: {bid_price}, ask: {ask_price}, price: {price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        # Assuming we are comparing the first two stocks in the quotes list
+        stock_names = list(prices.keys())
+        if len(stock_names) >= 2:
+            ratio = getRatio(prices[stock_names[0]], prices[stock_names[1]])
+            print(f"Ratio {stock_names[0]}/{stock_names[1]} = {ratio}")
+        else:
+            print("Not enough stock data to compute the ratio.")
+
+        time.sleep(N)
+
+if __name__ == "__main__":
+    main()
+`
